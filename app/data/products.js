cfe79806a1d79d2f46567d6321e245d825eaddac
carpoolvenom <carpoolvenom@gmail.com>
2024-11-19 11:01:53 +0500

    first commit

---
+++ app/data/products.js
@@ -0,0 +1,58 @@
+export const products = [
+  {
+    id: 1,
+    name: "Vibrant Hoodie",
+    price: 80,
+    image:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjYle3muB2dIjEOOqVhfp-Za86xYL1joeiZEGUTCgbxioMgZkGykjUHQ1ZJ-5zXjc1RyRBV1-ql57ztgeisYXFii8c61FJOYWNiQFEmUHPqwSUN8XfPErTcg_fOM6KTvY49HZtIZwQ9Z_85Dqf7hjtRsd4dNfn_63vHld4gB-5cRjEJCCcCZYWkq6zB7e4/s320/Modern_Confidence_in_Pastel_Streetwear-removebg-preview.png",
+  },
+  {
+    id: 2,
+    name: "Camel Jacket",
+    price: 15,
+    image:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhH7Jb-qFMbo7A8AEWq2htyKIkcb2XugCBX3lyNa11YKuIMeHxm4e_bcyGFTVZxgEY5IufWhlZfwDIutwFx2dVNzJp7pnD6ggxoIerbRtkk1D8TwvvgsdnuY-e1dMp3uAeSnraeD3DTXlppqS25jdkI7UOaxuPydg_GnKadTcSNgMlrlvhlKzEZWJe_evs/s320/Young_Man_in_Orange_Jacket-removebg-preview.png",
+  },
+  {
+    id: 3,
+    name: "Pink Coat",
+    price: 20,
+    image:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgp-l94KqzNeAjTQlKFGqG3s-I248KQ8-elOATp0rPs2PjI3NmPCESZcoVRdfhYgZEdHYTMQs3CEtkMSA9Wd0AGnKKifainWrkegHSmfhb2DkyguJd3LSvVFyOyyFEqEvQKsD5T-8D0JeI2-YNMb5qHAMYtaF5eJ6X0K7qbBhgcCNNJ5HWqeTkZEEHoR6I/s320/Stylish_Woman_in_Pink_Coat-removebg-preview.png",
+  },
+  {
+    id: 4,
+    name: "Leather Handbag",
+    price: 25,
+    image:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhj2pWPjJAPYu-n9lj2EGm9MBU8yfG9VMr9gpDwl_SChwnhBtcyXS8PukXMUWl6p4Q7R1Y0eceMgowibc-Nc37LjOjY63SetAwdExIQCfDbFqvo7bPBPohYQUjT8bX2vWiY7jPseO2AIgUJdF2_J3tNvHzykz3gfJuQ5Wbn_1Y_T_B3QwttCNnJq4IdSH8/s320/Vibrant_Orange_Leather_Handbag-removebg-preview.png",
+  },
+  {
+    id: 5,
+    name: "Orange Jacket",
+    price: 25,
+    image:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjc9KwnMpQ1MtGofN0UBrpJfwPELhBYjEu1JmWvfHH5Rdv5vlyQtyQXSmqgh0EkLtsjyVv7pTi_iDObKp9taqmOYUnUCjH0vEZnsn_w9guy1B84t4NRs6JmAMz4on8suQzSzV-0O8YLD2yQVpj9kJjnQEQ0WWOmUlGgLRXB2q0807dlKQAlB0JmYwNIHlI/s320/Stylish_Young_Man_in_Orange_Jacket-removebg-preview.png",
+  },
+  {
+    id: 6,
+    name: "Children Backpack",
+    price: 250,
+    image:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjY1S1K5r8lcSO734VaYvIro9eE2im7ce-_5-ODILIg6IzLfpXihZw4jaVrD56wcHpJXG5FBKoTaRM5q6vdv9DtrIMIMyfsHpkXSr5hlkstXBs8dSAm4ibw3sfbBaOWAQt1xSSw4neRelr1_gtWppkKFeS2EHhvYVXFxrXsHEfpjhmoCBXenSgghdzEbec/s320/Whimsical_Childrens_Backpack-removebg-preview.png",
+  },
+  {
+    id: 7,
+    name: "Vibrant Shirt",
+    price: 225,
+    image:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjVVvtxh72rHftgFf3axghXRvRKzR-xfMSuwkHy0kH-SSkASnLpXBdxLvM-jRdtrTsn3Ut0UXr2-eNnzWr3XGbn8lYOkzjfhWlTzvu2DoivoBQ7RiXZ_pw4_JvmTQmB6SSAOBsANqZVXRAhX8Zi_IDTt9t83GYPf0wQvHo1ZPhVEq8j_h8viRPYKMSs6ZA/s320/Confident_Man_in_Striped_Vibrant_Shirt-removebg-preview.png",
+  },
+  {
+    id: 8,
+    name: "Pink & Teal Hoodie",
+    price: 225,
+    image:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiQEEtNG1vRgIzb73pB6qqh44fcGi2fPKXR4oqsqDBbtvcxC68kDbrLQ0vQvjc1ttTI54Aig1NErOEwi2sZYoAVASn6GbiIdI26LbhGHDRmeu6e7AFgljsNpDzXvWy77iYUviVm910wMVwTyTOxbgLqiQuYjRNczqJzQwdCDkr8TPPD9q8b9oDmqooCOJM/s320/Urban_Chic_in_Pink_and_Teal-removebg-preview.png",
+  },
+];
---
+++ app/page.js
@@ -0,0 +1,5 @@
+"use client";
+
+export default function Home() {
+  return <h1>Next Store</h1>;
+}
